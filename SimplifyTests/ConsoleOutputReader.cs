4a575f10e4a6ae8d6f8dd290ac65a7e56dbb114d
Danila <mstr.danila@gmail.com>
2021-02-22 19:17:35 +0200

    Added tests checking console output; ConsoleOutputReader implemented;

---
+++ SimplifyTests/ConsoleOutputReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SimplifyTests
+{
+    public class ConsoleOutputReader : IDisposable
+    {
+        private readonly MemoryStream _memoryStream;
+        private readonly StreamReader _streamReader;
+        private readonly StreamWriter _streamWriter;
+        private long _writePosition = 0;
+        private long _readPosition = 0;
+
+        public ConsoleOutputReader()
+        {
+            _memoryStream = new MemoryStream();
+            _memoryStream.Seek(0, SeekOrigin.Begin);
+            _streamWriter = new StreamWriter(_memoryStream);
+            _streamReader = new StreamReader(_memoryStream);
+            Console.SetOut(_streamWriter);
+        }
+
+        public void Dispose()
+        {
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput())
+            {
+                AutoFlush = true
+            };
+            Console.SetOut(standardOutput);
+
+            _streamWriter.Dispose();
+            _streamReader.Dispose();
+            _memoryStream.Dispose();
+        }
+
+        public string ReadLine()
+        {
+            _streamWriter.Flush();
+            _writePosition = _memoryStream.Position;
+            _memoryStream.Position = _readPosition;
+            var line = _streamReader.ReadLine();
+            _readPosition = _memoryStream.Position;
+            _memoryStream.Position = _writePosition;
+            return line;
+        }
+    }
+}
--- SimplifyTests/ProgramTests.cs
+++ SimplifyTests/ProgramTests.cs
@@ -4,35 +4,44 @@ using System;
 
 namespace SimplifyTests
 {
+    [TestFixture]
     public class ProgramTests
     {
         [Test]
         public void EmptyArguments()
         {
+            using ConsoleOutputReader output = new ConsoleOutputReader();
             Program.Main(Array.Empty<string>());
+            Assert.AreEqual("Empty formula input.", output.ReadLine());
         }
 
         [Test]
         public void True()
         {
+            using ConsoleOutputReader output = new ConsoleOutputReader();
             Program.Main(new[] { "TRUE" });
+            Assert.AreEqual("Valid", output.ReadLine());
         }
 
         [Test]
         public void False()
         {
+            using ConsoleOutputReader output = new ConsoleOutputReader();
             Program.Main(new[] { "FALSE" });
+            Assert.AreEqual("Invalid", output.ReadLine());
         }
 
         [Test]
         public void OrTrueFalse()
         {
+            using ConsoleOutputReader output = new ConsoleOutputReader();
             Program.Main(new[] { "OR", "TRUE", "FALSE" });
         }
 
         [Test]
         public void OrA_B()
         {
+            using ConsoleOutputReader output = new ConsoleOutputReader();
             Program.Main(new[] { "OR", "a", "b" });
         }
     }
